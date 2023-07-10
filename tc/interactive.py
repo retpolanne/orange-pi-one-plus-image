08a4ca6165533c3959dbb74bcb295083b025e427
Anne Macedo <retpolanne@posteo.net>
2023-07-10 10:30:17 -0300

    Added testing

--- tc/interactive.py
+++ tc/interactive.py
@@ -1,6 +1,11 @@
 import tbot
+import time
 
 @tbot.testcase
 def test_uboot_dhcp() -> None:
     with tbot.ctx.request(tbot.role.BoardUBoot) as ub:
+        ub.exec0("regulator", "dev", "vcc-gmac-3v3")
+        ub.exec0("regulator", "enable")
+        ub.exec0("setenv", "autoload", "no")
+        time.sleep(5)
         ub.exec0("dhcp")
